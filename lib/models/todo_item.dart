3d3578536869f90b4ae1bd106905b47a11380283
Ken'ya Takemura <u770518g@ecs.osaka-u.ac.jp>
2025-05-21 21:17:59 +0900

    🚧 TODOリストとそれを管理するproviderの追加

---
+++ lib/models/todo_item.dart
@@ -0,0 +1,23 @@
+class TodoItem {
+  final String id; // UUIDv7
+  String title; // TODOの内容
+  bool isCompleted;
+
+  TodoItem({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  TodoItem complete() {
+    return TodoItem(id: id, title: title, isCompleted: true);
+  }
+
+  TodoItem uncomplete() {
+    return TodoItem(id: id, title: title, isCompleted: false);
+  }
+
+  TodoItem toggle() {
+    return TodoItem(id: id, title: title, isCompleted: !isCompleted);
+  }
+}
---
+++ lib/providers/todo_list_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:studying_flutter/models/todo_item.dart';
+import 'package:uuid/uuid.dart';
+
+class TodoListModel extends ChangeNotifier {
+  final List<TodoItem> _todoItems = [];
+
+  List<TodoItem> get TodoItems => _todoItems;
+
+  void addTodo(String title) {
+    const uuid = Uuid();
+    final newTodo = TodoItem(id: uuid.v7(), title: title);
+    _todoItems.add(newTodo);
+    notifyListeners();
+  }
+
+  void toggleTodoStatus(String id) {
+    final index = _todoItems.indexWhere((todo) => todo.id == id);
+    if (index != -1) {
+      _todoItems[index] = _todoItems[index].toggle();
+      notifyListeners();
+    }
+  }
+
+  void removeTodo(String id) {
+    _todoItems.removeWhere((todo) => todo.id == id);
+  }
+}
